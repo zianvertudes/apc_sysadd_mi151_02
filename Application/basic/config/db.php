caafbf04ba3af122d27557d3486fd2265c09a60f
zianvertudes <ziandenisse@gmail.com>
2017-08-01 11:08:15 +0800

    Fixed Gii

--- Application/basic/config/db.php
+++ Application/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cmsdb',
+    'dsn' => 'mysql:host=localhost;dbname=TicketingManagementSystem',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',

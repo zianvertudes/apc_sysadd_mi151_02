9f455c7dae1d6d577a826b677c2374b7695d1e9e
zianvertudes <ziandenisse@gmail.com>
2017-08-01 10:14:29 +0800

    Edited db.php

--- Application/basic/config/db.php
+++ Application/basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=cmsdb',
+    'username' => 'sysaddcms',
+    'password' => 'sysadd1and2',
     'charset' => 'utf8',
 ];

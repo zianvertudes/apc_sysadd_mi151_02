0bfccb1967f7a81334a5104360dafff7af536f0f
zianvertudes <ziandenisse@gmail.com>
2017-08-08 12:37:18 +0800

    Edited Site

--- Application/basic/views/site/index.php
+++ Application/basic/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Ticks Management System</h1>
+        <h1>Tickets Management System</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p> 
 

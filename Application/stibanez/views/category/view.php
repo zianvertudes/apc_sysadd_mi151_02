12340cc90b02db3849538a457a2279d359a1c368
Shaun Ibanez <stibanez@student.apc.edu.ph>
2017-08-08 21:53:18 +0800

    Modified category/view.php

--- Application/stibanez/views/category/view.php
+++ Application/stibanez/views/category/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Category */
 
-$this->title = $model->category_id;
+$this->title = $model->category_name;
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>

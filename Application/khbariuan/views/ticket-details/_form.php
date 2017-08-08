4ebce2f63b213d5c0e1fa807e281b6e737475670
Krisper Bariuan <khbariuan@gmail.com>
2017-08-08 21:54:54 +0800

    Trial.

---
+++ Application/khbariuan/views/ticket-details/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\TicketDetails */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-details-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'ticket_det_id')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'time_start')->textInput() ?>
+
+    <?= $form->field($model, 'time_end')->textInput() ?>
+
+    <?= $form->field($model, 'desc')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'emp_resp_id')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'ticket_id')->textInput() ?>
+
+    <?= $form->field($model, 'dept_resp_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>

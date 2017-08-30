c222a8a135e9825629756268721b5030b87c23fe
bytalosig <buenalexeytalosig@gmail.com>
2017-08-30 02:38:30 +0200

    Created CheckIn Customer ID dropDownList

--- Application/basic/views/check-in/_form.php
+++ Application/basic/views/check-in/_form.php
@@ -1,7 +1,11 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 use yii\widgets\ActiveForm;
+use app\models\CheckIn;
+use app\models\Customer1;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\CheckIn */
@@ -12,7 +16,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+
+	<?= $form->field($model, 'customer_id')->dropDownList(
+		ArrayHelper::map (Customer1::find()->all(), 'id', 'customer_name'),
+		['prompt'=>'Select Customer ID']
+		) ?>
 
     <?= $form->field($model, 'room_id')->textInput() ?>
 
